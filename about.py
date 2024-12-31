e0a9ebea359ef304a0e90103669bd5e7e3fe0f2d
Safira Nur Azkia <firaaasafira98@gmail.com>
2024-12-31 20:46:08 +0700

    Add files via upload
    MoveMaster App
    Safira Nur Azkia
    TI10 
    Projek UAS DDP

---
+++ about.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+st.title("About Move Master")
+st.image("logoddp.jpg")
+st.write("What is MoveMaster app?")
+st.write("Move Master kami dirancang untuk memudahkan orang-orang untuk menentukan estimasi waktu, kecepatan maupun jarak yang akan kita tempuh. ")
+
+st.subheader("Aplikasi ini di rancang oleh:")
+st.write("1. Ayu Nur Intany", )
+st.write("2. Maulidya Syifa Azzahra")
+st.write("3. Safira Nur Azkia")
